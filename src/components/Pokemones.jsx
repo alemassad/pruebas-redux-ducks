ceca6d9da916281d6c2acb6674ec5d2e9d441825
Luis massad <massadalejandro@gmail.com>
2022-11-11 16:04:30 -0300

    ducks-redux

---
+++ src/components/Pokemones.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {useDispatch, useSelector} from 'react-redux'
+import { obtenerPokemonesAccion, siguientePokemonAccion } from "../redux/pokeDucks";
+
+const Pokemones = () => {
+    const dispatch = useDispatch()
+
+    const pokemones = useSelector(store => store.pokemones.array)
+    
+    return (
+        <div>
+            Lista de pokemones
+            <button onClick={()=>dispatch(obtenerPokemonesAccion())}>Get Pokemones</button>
+            <button onClick={()=>dispatch(siguientePokemonAccion(20))}>Siguiente</button>
+            <ul> 
+                {
+                    pokemones.map(item => (
+                        <li key={item.name}>{item.name} </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+export default Pokemones
